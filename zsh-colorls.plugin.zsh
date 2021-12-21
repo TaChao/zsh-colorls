4eba54495b90c349e746caae9fb371d204f8d956
tachao <363646130@qq.com>
2021-12-21 16:01:30 +0800

    Update zsh-colorls.plugin.zsh

--- zsh-colorls.plugin.zsh
+++ zsh-colorls.plugin.zsh
@@ -4,13 +4,13 @@
 if hash exa 2>/dev/null
 then
     alias lc='exa'                               # exa with no options
-    alias l='exa -l --group-directories-first'                 # List
-    alias ll='exa -la  --git --group-directories-first'  # List, show almost all files (excludes ./ and ../)
-    alias la='exa -la --group-directories-first'               # List, show all files
-    alias lt='exa -l -t modified --git'             # List, sort by modification time (newest first)
-    alias lS='exa -l -s size --git'             # List, sort by size (largest first)
-    alias lr='exa --tree'                      # Show tree heirarchy, capped at depth 5 just in case
-    alias lx='exa -la -s extension --git'             # List, Sort by file type
+    alias l='exa -l --icons --group-directories-first'                 # List
+    alias ll='exa -la  --git --icons --group-directories-first'  # List, show almost all files (excludes ./ and ../)
+    alias la='exa -la --icons --group-directories-first'               # List, show all files
+    alias lt='exa -l --icons -t  modified --git'             # List, sort by modification time (newest first)
+    alias lS='exa -l --icons -s size --git'             # List, sort by size (largest first)
+    alias lr='exa --tree --icons'                      # Show tree heirarchy, capped at depth 5 just in case
+    alias lx='exa -la --icons -s extension --git'             # List, Sort by file type
 else
     alias ls='printf "Please install exa to enable the zsh-exa plugin\n\n" && ls --color=tty'
 fi
